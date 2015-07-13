d32ececda18b57454e569f51566dc1fac0ee3725
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:48:20 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_burlington_transit_bus/BurlingtonTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_burlington_transit_bus/BurlingtonTransitBusAgencyTools.java
@@ -37,7 +37,7 @@ public class BurlingtonTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Burlington Transit bus data...\n");
+		System.out.printf("\nGenerating Burlington Transit bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
