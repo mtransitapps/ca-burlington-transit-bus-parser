2f0d3480d88b2abff379453907f7bb2a78906670
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-14 17:02:28 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_burlington_transit_bus/BurlingtonTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_burlington_transit_bus/BurlingtonTransitBusAgencyTools.java
@@ -1,25 +1,12 @@
 package org.mtransit.parser.ca_burlington_transit_bus;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
-import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
-import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 // https://navburl-burlington.opendata.arcgis.com/pages/data
 // https://navburl-burlington.opendata.arcgis.com/search?tags=Transportation
@@ -28,55 +15,19 @@ import java.util.regex.Pattern;
 // http://opendata.burlington.ca/gtfs-rt/GTFS_Data.zip
 public class BurlingtonTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-burlington-transit-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new BurlingtonTransitBusAgencyTools().start(args);
 	}
 
-	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating Burlington Transit bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating Burlington Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
-	}
-
-	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
-		}
-		return super.excludeTrip(gTrip);
+	public String getAgencyName() {
+		return "Burlington Transit";
 	}
 
 	@NotNull
@@ -85,34 +36,14 @@ public class BurlingtonTransitBusAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
-	private static final Pattern DIGITS = Pattern.compile("[\\d]+");
-
-	private static final String A = "A";
-	private static final String B = "B";
-	private static final String X = "X";
-
-	private static final long RID_ENDS_WITH_A = 1000L;
-	private static final long RID_ENDS_WITH_B = 2000L;
-	private static final long RID_ENDS_WITH_X = 24000L;
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
 
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		String routeId = gRoute.getRouteShortName();
-		if (routeId.length() > 0 && Utils.isDigitsOnly(routeId)) {
-			return Integer.parseInt(routeId); // using stop code as stop ID
-		}
-		Matcher matcher = DIGITS.matcher(routeId);
-		if (matcher.find()) {
-			int digits = Integer.parseInt(matcher.group());
-			if (routeId.endsWith(A)) {
-				return RID_ENDS_WITH_A + digits;
-			} else if (routeId.endsWith(B)) {
-				return RID_ENDS_WITH_B + digits;
-			} else if (routeId.endsWith(X)) {
-				return RID_ENDS_WITH_X + digits;
-			}
-		}
-		throw new MTLog.Fatal("Can't find route ID for %s!", gRoute);
+	public boolean useRouteShortNameForRouteId() {
+		return true;
 	}
 
 	@NotNull
@@ -120,6 +51,11 @@ public class BurlingtonTransitBusAgencyTools extends DefaultAgencyTools {
 	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, routeLongName, getIgnoredWords());
 		return CleanUtils.cleanLabel(routeLongName);
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
 	private static final String AGENCY_COLOR = "006184"; // BLUE
@@ -131,21 +67,8 @@ public class BurlingtonTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
-	}
-
-	@Override
 	public boolean directionFinderEnabled() {
 		return true;
-	}
-
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Unexpected trips to merge %s and %s.", mTrip, mTripToMerge);
 	}
 
 	@NotNull
